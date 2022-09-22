7fa8d94b2e202b665de3d1cabdbe7d209f0f2fe4
EC2 Default User <ec2-user@ip-172-31-15-103.us-west-1.compute.internal>
2022-09-22 10:01:41 +0000

    memo

---
+++ memo.rb
@@ -0,0 +1,32 @@
+require "csv"
+
+
+puts "1（新規でメモを書く）２（既存のメモを上書きする）"
+type = gets.to_s
+memo_type = type.chomp
+
+if memo_type == "1"
+  puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
+  file_name = gets.chomp
+  
+  puts "メモの内容を記入してください。Ctrl + dで保存します。"
+  imput_memo = STDIN.read
+  memo = imput_memo.chomp
+  
+  CSV.open("#{file_name}.csv" , "w") do |csv|
+    csv.puts ["#{memo}"]
+  end
+
+elsif memo_type == "2"
+  puts "既存のメモを上書きします。上書きしたいメモの拡張子を除いたファイル名を入力してください。"
+  file_name = gets.chomp
+  
+  puts "メモの内容を記入してください。Ctrl + dで保存します。"
+  imput_memo = STDIN.read
+  memo = imput_memo.chomp
+  
+  CSV.open("#{file_name}.csv" , "w") do |csv|
+    csv.puts ["#{memo}"]
+  end
+  
+end
